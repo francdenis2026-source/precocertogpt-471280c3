f3ca8ffe7e6e74a32cc2dace284b3907333fee2a
francdenis2026-source <francdenis2026@gmail.com>
2026-08-15 09:32:28 -0500

    style: load light hero refinement

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "@fontsource-variable/outfit";
 import "./index.css";
 import "./styles/GlobalScrollbarRefinement.css";
 import "./styles/AuthCompactProfessional.css";
+import "./styles/HomeLightHeroRefinement.css";
 import App from "./App";
 import { HourlyHomeProductRotation } from "./components/HourlyHomeProductRotation";
 import { HomepageSearchInputPolish } from "./components/HomepageSearchInputPolish";

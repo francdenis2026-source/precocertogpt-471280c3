9a8b6e169ba8930f53aeeab83d2cf0ff3d7fba50
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-22 21:57:18 +0000

    deploy: caaa79d674716aa3c4f2743019b9f4088c264a27

--- assets/package-plus-B0BHUe49.js
+++ assets/package-plus-B0BHUe49.js
@@ -1,4 +1,4 @@
-import{c as a}from"./main-hWSUBCTQ.js";/**
+import{c as a}from"./main-CJeI71rw.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.

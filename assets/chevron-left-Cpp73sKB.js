fb1c7d97b783812e3a5279723a8c220e6e218043
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-23 04:23:36 +0000

    deploy: 8fe280568442db2a6fe967b111ccf8e6e46d5a28

--- assets/chevron-left-Cpp73sKB.js
+++ assets/chevron-left-Cpp73sKB.js
@@ -1,4 +1,4 @@
-import{n as e}from"./main-CDNm5N7z.js";/**
+import{n as e}from"./main-CvqBj7YE.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.

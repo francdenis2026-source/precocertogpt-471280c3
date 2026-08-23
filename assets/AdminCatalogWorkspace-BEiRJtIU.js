e2c6b6a67ee0fc5446efe36e54cfee586c6bc1cb
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-23 03:47:47 +0000

    deploy: 5a3c39d93a99d971addfe98f2e66f3bb91f4fd83

--- assets/AdminCatalogWorkspace-BEiRJtIU.js
+++ assets/AdminCatalogWorkspace-BEiRJtIU.js
@@ -1,4 +1,4 @@
-import{n as le,r as o,q as X,u as oe,aj as O,j as e,a3 as L,S as ce,af as de,a4 as R,e as T,T as D,B as me,ag as H,ah as J,L as f,ad as A,A as ue,ac as he,c as Q,z as xe,ae as je,ai as ge,a6 as B,a1 as V,s as W,am as pe,an as be,ao as fe,ak as I,al as ve,ap as Ne,aq as Se,ar as we,k as Ce,l as ye}from"./main-B1h-6kKQ.js";import{P as M}from"./package-plus-CPV-FF5M.js";import{C as _e}from"./chart-column-FVsggicX.js";import{P as $}from"./pencil-BQ7aOicx.js";import{T as F}from"./trash-2-BERTjYJB.js";/**
+import{n as le,r as o,q as X,u as oe,aj as O,j as e,a3 as L,S as ce,af as de,a4 as R,e as T,T as D,B as me,ag as H,ah as J,L as f,ad as A,A as ue,ac as he,c as Q,z as xe,ae as je,ai as ge,a6 as B,a1 as V,s as W,am as pe,an as be,ao as fe,ak as I,al as ve,ap as Ne,aq as Se,ar as we,k as Ce,l as ye}from"./main-BDW5Y8QY.js";import{P as M}from"./package-plus-BXq7Qd6p.js";import{C as _e}from"./chart-column-CUfnQC99.js";import{P as $}from"./pencil-BEl4gFdW.js";import{T as F}from"./trash-2-DOOU3I80.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.

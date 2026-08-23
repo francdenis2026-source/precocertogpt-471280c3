4a6f9c70a28845b3cfbe5101af1d32344949eca5
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-23 23:47:33 +0000

    deploy: 1c29cf507560c3eb255f15ebdbb35ee6bce11532

--- assets/package-plus-CWZl0oEd.js
+++ assets/package-plus-CWZl0oEd.js
@@ -1,4 +1,4 @@
-import{n as a}from"./main-bkNroRQv.js";/**
+import{n as a}from"./main-CQiCrIPy.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.

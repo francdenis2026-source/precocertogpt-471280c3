b7801cbbba95979108953b55844e108c8f41ab2f
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-25 01:09:25 +0000

    deploy: 2cc9dfb954044dae4e35c1f7b2c28e81bb871e50

--- assets/chevron-left-BYZjGIK4.js
+++ assets/chevron-left-BYZjGIK4.js
@@ -1,4 +1,4 @@
-import{n as e}from"./main-B48WE3HI.js";/**
+import{n as e}from"./main-BV9rdgky.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/chevron-right-D8js_-5Q.js
+++ assets/chevron-right-D8js_-5Q.js
@@ -1,4 +1,4 @@
-import{n as o}from"./main-B48WE3HI.js";/**
+import{n as o}from"./main-BV9rdgky.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.

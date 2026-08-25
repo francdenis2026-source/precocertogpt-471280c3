ae15be4f50b1fe9765dd704077a8354200153084
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-25 02:24:08 +0000

    deploy: 39517d5cf27defd41f8a4ee0b80489727a3a6df9

--- assets/pencil-BrhSd9Bd.js
+++ assets/pencil-BrhSd9Bd.js
@@ -1,4 +1,4 @@
-import{n as a}from"./main-CPwLV5Zs.js";/**
+import{n as a}from"./main-C_-MNr3G.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.

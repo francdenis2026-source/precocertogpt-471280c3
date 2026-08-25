b63ff105efca2dd9d1762ad108ee7be4134cbf6b
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-25 00:34:01 +0000

    deploy: c80a26d99e037a8cd77c9d960df889544f5d19d1

--- assets/chevron-left-CWw-kW2E.js
+++ assets/chevron-left-CWw-kW2E.js
@@ -1,4 +1,4 @@
-import{n as e}from"./main-gFuoJNkI.js";/**
+import{n as e}from"./main-BQ0gX-Is.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.

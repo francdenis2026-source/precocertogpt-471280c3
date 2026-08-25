bfb1b1e742cfc6f28bae56509333bdbca9d82b2a
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-25 13:31:46 +0000

    deploy: 97c571da3bad9b3024bbf6cbb07e35b0f1235f87

--- assets/chevron-left-BvzPAOpo.js
+++
@@ -1,6 +0,0 @@
-import{o}from"./main-CIOWUvei.js";/**
- * @license lucide-react v1.30.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],c=o("chevron-left",e);export{c as C};

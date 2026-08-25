da9c08bb17e2bbd7b68259caf9ccf56aeec4ed23
francdenis2026-source <francdenis2026-source@users.noreply.github.com>
2026-08-25 03:35:17 +0000

    deploy: e3d2c423f2c44a262e448c99f03e9654c4268438

--- assets/chart-column-CYYyS98d.js
+++ assets/chart-column-CYYyS98d.js
@@ -1,4 +1,4 @@
-import{n as t}from"./main-DW_SUYN8.js";/**
+import{n as t}from"./main-BPAzE8hJ.js";/**
  * @license lucide-react v1.30.0 - ISC
  *
  * This source code is licensed under the ISC license.
